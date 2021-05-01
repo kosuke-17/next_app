1e148f54623aff73fbc6b807452556c455da4f78
kosuke-17 <tamutamu17@icloud.com>
2021-05-01 11:47:15 +0900

    複数ページの作成

--- pages/other.js
+++ pages/other.js
@@ -1,8 +1,8 @@
 import Header from './header'
 import Link from 'next/link'
 
-export default function Home() {
-  const title = "Index"
+export default function Other() {
+  const title = "Other"
 
   return (
     <div>
@@ -10,13 +10,13 @@ export default function Home() {
       <h1 className="bg-primary px-3 text-white display-4 text-right">React</h1>
       <div className="container">
         <h3 className="my-3 text-primary text-center">{title}</h3>
-        <div className="card p-3 text-center">
-          <p>これはページ移動のサンプルです</p>
-          <Link href="/other">
-            <a>Go to Other page &gt;&gt;</a>
+        <div className="card p-3">
+          <p>これはもう一つのページの表示です</p>
+          <Link href="/">
+            <a>&lt;&lt; Back to INdex page</a>
           </Link>
         </div>
-      </div>
+     </div>
     </div>
   )
 }
