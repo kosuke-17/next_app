105fccd0a856668905f67c48cf89b8d92a62c416
kosuke-17 <tamutamu17@icloud.com>
2021-05-01 21:36:17 +0900

    Firebaseパッケージのインストール

--- components/data.js
+++ components/data.js
@@ -2,6 +2,6 @@ export default [
   {"name": "taro", "mail": "taro@yamada", "age": 39},
   {"name": "hanako", "mail": "hanako@flower", "age": 28},
   {"name": "sachiko", "mail": "sachiko@happy", "age": 17},
-  {"name": "jiro", "mail": "jro@change", "age": 6},
+  {"name": "jiro", "mail": "jiro@change", "age": 6},
   {"name": "mami", "mail": "mami@mumemo", "age": 50},
 ]
---
+++ components/fire.js
@@ -0,0 +1,10 @@
+// import firebase from 'firebase'
+
+// // 各プロジェクトの設定を記述
+// const firebaseConfig = {
+//   apikey: "･･････APIキー･･････",
+//   authDomain: "プロジェクト名.firebaseapp.com",
+//   databaseURL: "データベースのURL",
+//   projectId: "プロジェクト名",
+//   storageBucket: "プロジェクト名.appspot.com",
+// }
