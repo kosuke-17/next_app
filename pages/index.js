efe3ea37304775d70f91acaf760d9a079371f69c
kosuke-17 <tamutamu17@icloud.com>
2021-05-01 13:28:30 +0900

    useSWRでステートを用意

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,37 @@
-import {useState} from 'react'
 import Layout from '../components/layout'
+import useSWR from 'swr'
 import Link from 'next/link'
 
 
 export default function Home() {
-  const url = './data.json'
-  const [data, setData] = useState({message:'', data:[]})
 
-  fetch(url)
-  .then(res=> res.json())
-  .then(res=> setData(res))
+  const {data} = useSWR('/data.json')
 
   return (
     <div>
       <Layout header="Next.js" title="Top page.">
       <div className="alert alert-primary text-center">
-        <h5 className="mb-4">{data.message}</h5>
-        <table className="table bg-white">
-          <thead className="table-dark">
+        <h5 className="mb-4">{data != undefined ? data.message : 'error...' }</h5>
+        <table className="table table-dark">
+          <thead className="">
             <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
           </thead>
           <tbody>
-            {data.data.map((value, key)=> (
+            {data != undefined ? data.data.map((value, key)=> (
               <tr key={key}>
                 <th>{value.name}</th>
                 <td>{value.mail}</td>
                 <td>{value.age}</td>
               </tr>
-            ))}
+            )) : <tr><th></th><td>no data.</td><td></td></tr>}
           </tbody>
         </table>
       </div>
       <Link href="./other">
-          <button className="btn btn-primary">
-            &lt;&lt; Go to other page
-          </button>
-        </Link>
+        <button className="btn btn-primary">
+          &lt;&lt; Go to other page
+        </button>
+      </Link>
       </Layout>
     </div>
   )
