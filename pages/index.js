fcbd4c38716b99f6c6fecfa6a2381d03baaded77
kosuke-17 <tamutamu17@icloud.com>
2021-05-01 14:26:39 +0900

    [...params].jsを利用したデータ取得

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,14 @@ import Link from 'next/link'
 
 
 export default function Home() {
-  const [ address, setAddress] = useState('/api/hello')
+  const [pref, setPref] = useState({id:0, item:'name'})
+  const [ address, setAddress ] = useState('/api/hello/' + pref.id + '/' + pref.item)
   const {data, err } = useSWR(address)
 
   const onChange = (e)=> {
-    setAddress('/api/hello/' + e.target.value)
+    pref.id = e.target.value
+    setPref(pref)
+    setAddress('/api/hello/' + pref.id + '/' + pref.item)
   }
 
   return (
@@ -17,7 +20,12 @@ export default function Home() {
       <Layout header="Next.js" title="Top page.">
       <div className="alert alert-primary text-center">
         <h5 className="mb-4">{ JSON.stringify(data) }</h5>
-        <input type="number" className="form-control" onChange={onChange} />
+        <input type="number" className="form-control form-control-sm mb-2" onChange={onChange} />
+        <select onChange={onChange} className="form-control form-control-sm">
+          <option value="name">Name</option>
+          <option value="mail">Mali</option>
+          <option value="age">Age</option>
+        </select>
       </div>
       <Link href="./other">
         <button className="btn btn-primary">
