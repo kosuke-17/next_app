fd47d7bc8f2317f569a763956c02d9b8efed6b28
kosuke-17 <tamutamu17@icloud.com>
2021-05-01 12:48:09 +0900

    imageコンポーネントを利用

--- components/image.js
+++ components/image.js
@@ -1,8 +1,8 @@
-// export default function MyImage(props) {
-//   let fname = './' + props.fname
-//   let size = props.size + "px"
+export default function MyImage(props) {
+  let fname = './' + props.fname
+  let size = props.size + "px"
 
-//   return (
-//     <img width={size} border="1" src={fname} />
-//   )
-// }
+  return (
+    <img width={size} border="1" src={fname} />
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Layout from '../components/layout'
+import MyImage from '../components/image'
 import Link from 'next/link'
+
 
 export default function Home() {
   return (
@@ -7,12 +9,13 @@ export default function Home() {
       <Layout header="Next.js" title="Top page.">
       <div className="alert alert-primary text-center">
         <h5 className="mb-4">Welcome to next.js!</h5>
-        <Link href="./other">
+        <MyImage fname="image.jpg" size="300" />
+      </div>
+      <Link href="./other">
           <button className="btn btn-primary">
             go to Other &gt;&gt;
           </button>
         </Link>
-      </div>
       </Layout>
     </div>
   )
