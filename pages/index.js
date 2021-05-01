96d878262d578bb0b48a99b4f485fd245aa4ff8a
kosuke-17 <tamutamu17@icloud.com>
2021-05-01 12:36:23 +0900

    レイアウトの利用

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,19 @@
-import Header from './header'
+import Layout from '../components/layout'
 import Link from 'next/link'
 
 export default function Home() {
-  const title = "Index"
-
   return (
     <div>
-      <Header title={title} />
-      <h1 className="bg-primary px-3 text-white display-4 text-right">React</h1>
-      <div className="container">
-        <h3 className="my-3 text-primary text-center">{title}</h3>
-        <div className="card p-3 text-center">
-          <p>これはページ移動のサンプルです</p>
-          <Link href="/other">
-            <a>Go to Other page &gt;&gt;</a>
-          </Link>
-        </div>
-        <div className="card p-3">
-          <p>これはもう一つのページの表示です</p>
-          <Link href="/">
-            <a>&lt;&lt; Back to Index page</a>
-          </Link>
-        </div>
+      <Layout header="Next.js" title="Top page.">
+      <div className="alert alert-primary text-center">
+        <h5 className="mb-4">Welcome to next.js!</h5>
+        <Link href="./other">
+          <button className="btn btn-primary">
+            go to Other &gt;&gt;
+          </button>
+        </Link>
       </div>
+      </Layout>
     </div>
   )
 }
--- pages/other.js
+++ pages/other.js
@@ -1,27 +1,19 @@
-import Header from './header'
+import Layout from '../components/layout'
 import Link from 'next/link'
 
 export default function Other() {
-  const title = "Other"
-
   return (
     <div>
-      <Header title={title} />
-      <h1 className="bg-primary px-3 text-white display-4 text-right">React</h1>
-      <div className="container">
-        <h3 className="my-3 text-primary text-center">{title}</h3>
-        <div className="card p-3">
-          <p>これはもう一つのページの表示です</p>
-          <Link href="/">
-            <a>&lt;&lt; Back to Index page</a>
-          </Link>
-        </div>
-        <div className="card p-3">
-          <Link href="/">
-          <i class="fab fa-amilia">トップページへ</i>
-          </Link>
-        </div>
-     </div>
+      <Layout header="Next.js" title="Other page.">
+      <div className="card p-4 text-center">
+        <h5 className="mb-4">This is Other page...</h5>
+        <Link href=".">
+          <button className="btn btn-primary">
+            &lt;&lt; Back to Index page
+          </button>
+        </Link>
+      </div>
+      </Layout>
     </div>
   )
 }
